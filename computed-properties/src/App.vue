183fa344facf5d650e2b856e929c010b00399e43
Igor Luíz <igor@devstacks.com.br>
2016-09-21 14:23:35 -0300

    Computed properties

---
+++ computed-properties/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      first: 'Igor',
+      last: 'Luíz'
+    }
+  },
+  mounted () {
+    setTimeout(() => this.name = 'Halfeld Luíz', 2000)
+  },
+  computed: {
+    name: {
+      get () {
+        return `${this.first} ${this.last}`
+      },
+
+      set (v) {
+        const names = v.split(' ')
+        this.first = names[0]
+        this.last = names[1]
+      }
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
